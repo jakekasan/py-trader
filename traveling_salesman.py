a9a515e37abd77ec5965544237bec9bb567d922f
jakekasan <jake.kasan@gmail.com>
2018-05-17 15:02:19 +0100

    debugged some things from GA

--- traveling_salesman.py
+++ traveling_salesman.py
@@ -2,9 +2,11 @@ import random
 import math
 import sys
 
+pop_size = int(sys.argv[1])
 cities = []
 for _ in range(int(sys.argv[1])):
     cities.append([random.randint(-10,10),random.randint(-10,10)])
+
 mutation_rate = 0.01
 
 
@@ -53,13 +55,16 @@ def runGA(cities,pop_size,mut_rate):
     while(True):
         pop = reproducePopulation(pop,mut_rate)
         gens += 1
+        print(gens)
         counter += 1
-
+        if counter >= 25:
+            printStatus(pop,gens)
+            counter = 0
     return
 
 def printStatus(pop,gens):
     min_dist,index = getDistIndex(pop)
-    print("Distance")
+    print("Total distance: {}\nPoints: {}".format(min_dist,pop[index]))
 
 def getDistIndex(pop):
     listmap = list(map(getDistance,pop))
@@ -73,35 +78,52 @@ def getDistIndex(pop):
 def getInitialPopulation(cities,pop_size):
     pop = []
     for _ in range(pop_size):
-        pop.append(randon.sample(cities,len(cities)))
+        pop.append(random.sample(cities,len(cities)))
     return pop
 
 def reproducePopulation(pop,mutation_rate):
+    print("reproducing:")
+    for x in pop:
+        print(x)
     mating_pool = []
     new_pop = []
     min_dist = min(list(map(getDistance,pop)))
+    print("Pre mating pool",([] in mating_pool))
     for cities in pop:
         fitness = int(min_dist/(getDistance(cities))*100)
+        print(fitness)
         for i in range(fitness):
-            mating_pool.append(cities)
+            if cities == []:
+                print(cities)
+            else:
+                mating_pool.append(cities)
+    print("Post mating pool",([] in mating_pool))
     for _ in pop:
-        new_pop.append(reproduceTwo(random.choice(mating_pool),random.choice(mating_pool)))
+        print("Pre one",([] in mating_pool))
+        one = random.sample(mating_pool,1)[0]
+        print("Post one",([] in mating_pool))
+        two = random.sample(mating_pool,1)[0]
+        if one == [] or two == []:
+            print(([] in mating_pool))
+        new_pop.append(reproduceTwo(one,two))
     for i in range(len(new_pop)):
         if random.random() < mutation_rate:
             new_pop[i] = mutate(new_pop[i])
     return new_pop
 
 def reproduceTwo(one,two):
+    print(one)
+    print(two)
     newList = [0 for _ in one]
     a = random.randint(0,(len(one)-2))
     b = random.randint(a+1,(len(one)-1))
     if a == b:
         newList[a:(b+1)] = one[a:(b+1)]
-    for i in range(len(two)):
-        if (two[i] not in newList) and (i not in range(a,b+1)):
+    for i in [x for x in range((len(two)-1),0,-1)]:
+        if (two[i] not in newList) and (i not in [x for x in range(a,b+1)]):
             newList[i] = two.pop(i)
     while 0 in newList:
-        for i in range(len(newList)):
+        for i in [x for x in range(len(newList))]:
             if newList[i] == 0:
                 newList[i] = two.pop(0)
     return newList
@@ -124,4 +146,4 @@ def mutate(cities):
 
 
 
-run(cities)
+runGA(cities,pop_size,mutation_rate)
