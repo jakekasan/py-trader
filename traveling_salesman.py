f447b2f24f49567797d71b05ba803c6fdde38d3c
jakekasan <jake.kasan@gmail.com>
2018-05-17 13:31:26 +0100

    filled in functions for GA

--- traveling_salesman.py
+++ traveling_salesman.py
@@ -5,6 +5,8 @@ import sys
 cities = []
 for _ in range(int(sys.argv[1])):
     cities.append([random.randint(-10,10),random.randint(-10,10)])
+mutation_rate = 0.01
+
 
 def fact(n):
     if n < 2:
@@ -46,8 +48,27 @@ def run(cities):
 
 def runGA(cities,pop_size,mut_rate):
     pop = getInitialPopulation(cities,pop_size)
+    counter = 0
+    gens = 0
+    while(True):
+        pop = reproducePopulation(pop,mut_rate)
+        gens += 1
+        counter += 1
 
     return
+
+def printStatus(pop,gens):
+    min_dist,index = getDistIndex(pop)
+    print("Distance")
+
+def getDistIndex(pop):
+    listmap = list(map(getDistance,pop))
+    min_dist = min(listmap)
+    for i in range(len(listmap)):
+        if listmap[i] == min_dist:
+            return min_dist,i
+
+
 
 def getInitialPopulation(cities,pop_size):
     pop = []
@@ -55,9 +76,52 @@ def getInitialPopulation(cities,pop_size):
         pop.append(randon.sample(cities,len(cities)))
     return pop
 
-def reproducePopulation(pop):
+def reproducePopulation(pop,mutation_rate):
+    mating_pool = []
     new_pop = []
-    
+    min_dist = min(list(map(getDistance,pop)))
+    for cities in pop:
+        fitness = int(min_dist/(getDistance(cities))*100)
+        for i in range(fitness):
+            mating_pool.append(cities)
+    for _ in pop:
+        new_pop.append(reproduceTwo(random.choice(mating_pool),random.choice(mating_pool)))
+    for i in range(len(new_pop)):
+        if random.random() < mutation_rate:
+            new_pop[i] = mutate(new_pop[i])
+    return new_pop
+
+def reproduceTwo(one,two):
+    newList = [0 for _ in one]
+    a = random.randint(0,(len(one)-2))
+    b = random.randint(a+1,(len(one)-1))
+    if a == b:
+        newList[a:(b+1)] = one[a:(b+1)]
+    for i in range(len(two)):
+        if (two[i] not in newList) and (i not in range(a,b+1)):
+            newList[i] = two.pop(i)
+    while 0 in newList:
+        for i in range(len(newList)):
+            if newList[i] == 0:
+                newList[i] = two.pop(0)
+    return newList
+
+def mutate(cities):
+    ind = [x for x in range(len(cities))]
+    a = ind.pop(random.choice(ind))
+    b = random.choice(ind)
+
+    hold = cities[a]
+    cities[a] = cities[b]
+    cities[b] = hold
+
+    return cities
+
+
+
+
+
+
 
 
 run(cities)
